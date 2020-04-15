6cbb982001d60cba463c30bf2365904b878cc806
Sabsi <sabina.wana@yahoo.de>
2020-04-15 16:43:44 +0200

    'Ready'

--- app/static/client.js
+++ app/static/client.js
@@ -34,12 +34,10 @@ function analyze() {
       el("progressbar_1").innerHTML = `${response["results"][0][1]} %`;
       el("progressbar_2").innerHTML = `${response["results"][1][1]} %`;
       el("progressbar_3").innerHTML = `${response["results"][2][1]} %`;
-      el("progressbar_4").innerHTML = `${response["results"][3][1]} %`;
 
       el("label_1").innerHTML = `${response["results"][0][0]}`;
       el("label_2").innerHTML = `${response["results"][1][0]}`;
       el("label_3").innerHTML = `${response["results"][2][0]}`;
-      el("label_4").innerHTML = `${response["results"][3][0]}`;
 
       el("progressbar_1").setAttribute('style', 'width:'+Number(response["results"][0][1])+'%')
       el("progressbar_1").setAttribute('aria-valuenow', response["results"][0][1]);
@@ -47,8 +45,6 @@ function analyze() {
       el("progressbar_2").setAttribute('aria-valuenow', response["results"][1][1]);
       el("progressbar_3").setAttribute('style', 'width:'+Number(response["results"][2][1])+'%')
       el("progressbar_3").setAttribute('aria-valuenow', response["results"][2][1]);
-      el("progressbar_4").setAttribute('style', 'width:'+Number(response["results"][3][1])+'%')
-      el("progressbar_4").setAttribute('aria-valuenow', response["results"][3][1]);
     }
     el("analyze-button").innerHTML = "Analyze";
   };
